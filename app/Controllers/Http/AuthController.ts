2cce08c318068476dbd529a345511cb2daec8977
laianribas <laian67a@gmail.com>
2022-09-30 10:59:09 -0300

    initial_commit

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,23 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+import Hash from '@ioc:Adonis/Core/Hash'
+
+export default class AuthController {
+  public async store({ auth, request, response }: HttpContextContract) {
+    const email = request.input('email')
+    const password = request.input('password')
+
+    // Lookup user manually
+    const user = await User.query().where('email', email).firstOrFail()
+
+    // Verify password
+    if (!(await Hash.verify(user.password, password))) {
+      return response.unauthorized('Invalid credentials')
+    }
+
+    // Generate token
+    const token = await auth.use('api').generate(user)
+
+    return token
+  }
+}
